e0762ab33c6fee7b3515119d7cb8096933582648
agusbrata <agusbrata@gmail.com>
2018-11-14 15:48:50 +0700

    Update config.php

--- po-includes/core/config.php
+++ po-includes/core/config.php
@@ -10,7 +10,7 @@ define('CONF_VER', $site['ver']);
 define('CONF_BUILD', $site['build']);
 define('CONF_RELEASE', $site['release']);
 
-$site['url'] = "https://modalkamu.herokuapp.com/";
+$site['url'] = "https://modalkamu.herokuapp.com";
 $site['adm'] = "po-admin";
 $site['con'] = "po-content";
 $site['inc'] = "po-includes";
